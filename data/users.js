eb0b19f32de13696b17c6631c556649a3ca8b221
ada lovelace <williambookman@pursuit.org>
2024-03-07 19:07:44 -0500

    users data completed

--- data/users.js
+++ data/users.js
@@ -1,18 +1,85 @@
-let users = [];
+const { faker } = require("@faker-js/faker");
+
+let users = [
+{
+    id: faker.company.buzzVerb(),
+    name:faker.person.fullName()
+
+},
+{
+    id: 'envisioneer',
+    name: 'Arlene Jenkins'
+
+}
+];
 
 
-const findAll = () => {};
+
+const findAll = (users) => {
+    return users
+};
 
 
-const findById = (id) => {};
 
 
-const add = (user) => {};
+const findById = (id) => {
+    return users.find((user) => user.id === id);
+};
 
 
-const updateById = (id, updateInfo) => {};
+const add = (user) => {
+    const newId = faker.company.buzzVerb();
+    const newName = faker.person.fullName();
+ 
+
+    const newUser = {
+      id: newId,
+      name: newName
+    };
+  
+    users.push(newUser);
+    console.log('New user has been added')
+  
+    return newUser;
 
 
-const deleteById = (id) => {};
+};
+
+
+
+const updateById = (id, updateInfo) => {
+    const found = users.some((user) => user.id === id);
+    if (found) {
+      users.name = updateInfo.name;
+      console.log("user has been updated");
+      return users;
+    } else {
+      console.log("user not found");
+      return users;
+    }
+
+};
+
+const updateInfo = {
+    name: "Salvador Dali!",
+  };
+
+
+
+const deleteById = (id) => {
+
+    const found = users.some((user) => user.id === id);
+
+    if (found) {
+        users.filter((user) => user.id === id)
+        console.log("user has been deleted");
+        return users;
+      } else {
+        console.log("user not found. No action taken");
+        return users;
+      }
+};
+
+console.log(deleteById('envisioneer'))
 
 module.exports = { findAll, findById, add, updateById, deleteById };
